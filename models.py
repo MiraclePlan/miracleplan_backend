6066840134d259c066370a2c5d2720ecf6825cda
paul3083 <paul30833083@gmail.com>
2024-08-04 00:46:46 +0900

    models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import relationship
+from database import Base
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(150), unique=True, index=True)
+    hashed_password = Column(String(150))
+    todos = relationship("Todo", back_populates="owner")
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(150), unique=True, index=True)
+    description = Column(Text, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="todos")
