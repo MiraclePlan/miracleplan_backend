2c42bc8d8e14e94fd4110b2f3a69c8bb657df6d4
paul3083 <paul30833083@gmail.com>
2024-08-04 00:46:52 +0900

    schemas.py

---
+++ schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+class TokenRequest(BaseModel):
+    username: str
+    password: str
+
+class UserBase(BaseModel):
+    username: str
+
+class UserCreate(UserBase):
+    password: str
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class TodoBase(BaseModel):
+    title: str
+    description: str
+
+class TodoCreate(TodoBase):
+    pass
+
+class Todo(TodoBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
