abae20194d05cb8398e2fdfe0110b88a7c06c018
paul3083 <paul30833083@gmail.com>
2024-08-06 04:23:11 +0900

    models.py

--- models.py
+++ models.py
@@ -6,14 +6,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    username = Column(String(10), unique=True, index=True)
+    hashed_password = Column(String(255))
 
 class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
+    title = Column(String(10), index=True)
     start_date = Column(Date)
     end_date = Column(Date)
     completed = Column(Boolean, default=False)
@@ -24,5 +24,5 @@ class Group(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True)
+    name = Column(String(10), unique=True, index=True)
     members = relationship("User", back_populates="group")
