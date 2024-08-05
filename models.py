61a4eefac16b7fe5dca2ff8a34d7313b7f99fdbc
paul3083 <paul30833083@gmail.com>
2024-08-06 00:55:30 +0900

    models.py

--- models.py
+++ models.py
@@ -1,19 +1,21 @@
+from sqlalchemy import Boolean, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(150), unique=True, index=True)
-    hashed_password = Column(String(150))
-    todos = relationship("Todo", back_populates="owner")
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
 class Todo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(Text, nullable=False)
+    title = Column(String, index=True)
+    start_date = Column(Date)
+    end_date = Column(Date)
+    completed = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
-    owner = relationship("User", back_populates="todos")
+    owner = relationship("User")
