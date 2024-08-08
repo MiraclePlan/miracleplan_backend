f7a3ca7bc79497b1031c3a8f4c4dc75af9ad55d4
paul3083 <paul30833083@gmail.com>
2024-08-08 14:28:15 +0900

    database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://root:Blue3570!!@localhost/miracleplan'
+SQLALCHEMY_DATABASE_URL = 'mysql+mysqlconnector://paul3083:Blue3570!!@mysql-container:3306/miracleplan'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
