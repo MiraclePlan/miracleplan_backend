db4958aa17e192b7e91a43b268f688a315771c06
paul3083 <paul30833083@gmail.com>
2024-08-08 10:34:27 +0900

    models.py

--- models.py
+++ models.py
@@ -1,28 +1,27 @@
-from sqlalchemy import Boolean, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from database import Base
+
+group_membership = Table('group_membership', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('group_id', Integer, ForeignKey('groups.id'))
+)
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(10), unique=True, index=True)
-    hashed_password = Column(String(255))
-
-class Todo(Base):
-    __tablename__ = "todos"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(10), index=True)
-    start_date = Column(Date)
-    end_date = Column(Date)
-    completed = Column(Boolean, default=False)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-    owner = relationship("User")
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    groups = relationship("Group", secondary=group_membership, back_populates="members")
 
 class Group(Base):
     __tablename__ = "groups"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(10), unique=True, index=True)
-    members = relationship("User", back_populates="group")
+    name = Column(String, unique=True, index=True)
+    creator_id = Column(Integer, ForeignKey('users.id'))
+    members = relationship("User", secondary=group_membership, back_populates="groups")
+    creator = relationship("User", back_populates="created_groups")
+
+User.created_groups = relationship("Group", back_populates="creator")
